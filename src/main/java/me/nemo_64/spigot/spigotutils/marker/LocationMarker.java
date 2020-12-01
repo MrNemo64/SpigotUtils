7652f94e097880823ef1adc67e197b05301154fc
MrNemo64 <my.nemo64.otravez@gmail.com>
2020-12-01 23:07:37 +0100

    Buf fix in LocationMarker
    LocationMarker#markLocation(Player player, Location location, ChatColor color) was returning null when it isn't supposed to

--- src/main/java/me/nemo_64/spigot/spigotutils/marker/LocationMarker.java
+++ src/main/java/me/nemo_64/spigot/spigotutils/marker/LocationMarker.java
@@ -1,25 +1,25 @@
 /*
-MIT License
-
-Copyright (c) 2020 MrNemo64
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+ * MIT License
+ * 
+ * Copyright (c) 2020 MrNemo64
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
 */
 
 package me.nemo_64.spigot.spigotutils.marker;
@@ -48,7 +48,7 @@ import me.nemo_64.spigot.spigotutils.xseries.ReflectionUtils;
 /**
  * Utility class for marking block using shulkers and NMS
  * 
- * @version 2.1
+ * @version 2.1.1
  * @author MrNemo64
  */
 public class LocationMarker {
@@ -219,7 +219,7 @@ public class LocationMarker {
 		ShulkerMarker markerShulker = new ShulkerMarker(player);
 		Object shulker = createShulker(location.getWorld());
 		if (shulker == null)
-			return null;
+			return markerShulker;
 		Object idO = null;
 		Object uuidO = null;
 		try {
@@ -230,7 +230,7 @@ public class LocationMarker {
 			uuidO = entityShulkerGetUuidMethod.invoke(shulker);
 			Object dataWatcher = entityShulkerGetDataWatcherMethod.invoke(shulker);
 			if (dataWatcher == null)
-				return null;
+				return markerShulker;
 
 			Object packetPlayOutSpawnEntityLiving = packetPlayOutSpawnEntityLivingConstructor.newInstance(shulker);
 			Object packetPlayOutEntityMetadata = packetPlayOutEntityMetadataConstructor.newInstance(idO, dataWatcher, true);
